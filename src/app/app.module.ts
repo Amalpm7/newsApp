f9c7a6df21a1677667ab92d67b09fcfa9dce24a0
Amalpm7 <amal.p@nestgroup.net>
2022-10-10 23:13:59 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NewsAppComponent } from './news-app/news-app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NewsAppComponent
   ],
   imports: [
     BrowserModule,
