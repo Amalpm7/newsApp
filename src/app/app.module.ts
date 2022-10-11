37ca3a0b743887a90c90ea63c8575eb0b4557c3b
Amalpm7 <amal.p@nestgroup.net>
2022-10-11 14:34:53 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NewsAppComponent } from './news-app/news-app.component';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { NewsAppComponent } from './news-app/news-app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
